2f26552cbb9b9050d162dfe4bb227f96409cbd6c
Jakub Bukowski <littlefellapp@gmail.com>
2025-04-16 16:42:35 +0200

    Poprawiony test i zależności

--- DataTest/BallUnitTest.cs
+++ DataTest/BallUnitTest.cs
@@ -10,32 +10,28 @@
 
 namespace TP.ConcurrentProgramming.Data.Test
 {
-  [TestClass]
-  public class BallUnitTest
-  {
-    [TestMethod]
-    public void ConstructorTestMethod()
+    [TestClass]
+    public class BallUnitTest
     {
-      Vector testinVector = new Vector(0.0, 0.0);
-      Ball newInstance = new(testinVector, testinVector);
-    }
-    //[TestMethod]
-    //public void Niepowodzenie()
-    //{
-    //        Assert.AreEqual(2, 3);
-    //}
+        [TestMethod]
+        public void ConstructorTestMethod()
+        {
+            Vector testinVector = new Vector(0.0, 0.0);
+            Ball newInstance = new(testinVector, testinVector);
+            Assert.IsNotNull(newInstance);
+        }
 
         [TestMethod]
-    public void MoveTestMethod()
-    {
-      Vector initialPosition = new(10.0, 10.0);
-      Ball newInstance = new(initialPosition, new Vector(0.0, 0.0));
-      IVector curentPosition = new Vector(0.0, 0.0);
-      int numberOfCallBackCalled = 0;
-      newInstance.NewPositionNotification += (sender, position) => { Assert.IsNotNull(sender); curentPosition = position; numberOfCallBackCalled++; };
-      newInstance.Move(new Vector(0.0, 0.0));
-      Assert.AreEqual<int>(1, numberOfCallBackCalled);
-      Assert.AreEqual<IVector>(initialPosition, curentPosition);
+        public void MoveTestMethod()
+        {
+            Vector initialPosition = new(10.0, 10.0);
+            Ball newInstance = new(initialPosition, new Vector(0.0, 0.0));
+            IVector curentPosition = new Vector(0.0, 0.0);
+            int numberOfCallBackCalled = 0;
+            newInstance.NewPositionNotification += (sender, position) => { Assert.IsNotNull(sender); curentPosition = position; numberOfCallBackCalled++; };
+            newInstance.Move(new Vector(0.0, 0.0));
+            Assert.AreEqual<int>(1, numberOfCallBackCalled);
+            Assert.AreEqual<IVector>(initialPosition, curentPosition);
+        }
     }
-  }
 }
